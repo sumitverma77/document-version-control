eadf190cf71f7c358bc6022a5f7ce1e3a18b846a
sumitverma77 <987sumitverma@gmail.com>
2025-04-15 17:22:45 +0530

    setup

---
+++ src/test/java/com/security/documentversioncontrol/DocumentVersionControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.documentversioncontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentVersionControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
