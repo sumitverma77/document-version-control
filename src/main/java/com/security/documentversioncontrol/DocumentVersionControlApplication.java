da0d08156093adfbec2f5308869c3e7ac2be9d48
sumitverma77 <987sumitverma@gmail.com>
2025-04-21 17:20:57 +0530

    fix: dotenv bug

--- src/main/java/com/security/documentversioncontrol/DocumentVersionControlApplication.java
+++ src/main/java/com/security/documentversioncontrol/DocumentVersionControlApplication.java
@@ -1,6 +1,5 @@
 package com.security.documentversioncontrol;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
